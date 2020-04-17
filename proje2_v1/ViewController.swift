1bfbf376fd0ac38bdfa3bf605d285509a5b58de0
Ömer Alışkan <omerfarukaliskan@icloud.com>
2020-04-17 20:05:56 +0300

    Initial Commit

---
+++ proje2_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proje2_v1
+//
+//  Created by Ömer Faruk Alışkan on 17.04.20.
+//  Copyright © 2020 Ömer Faruk Alışkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ proje2_v1Tests/proje2_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  proje2_v1Tests.swift
+//  proje2_v1Tests
+//
+//  Created by Ömer Faruk Alışkan on 17.04.20.
+//  Copyright © 2020 Ömer Faruk Alışkan. All rights reserved.
+//
+
+import XCTest
+@testable import proje2_v1
+
+class proje2_v1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
