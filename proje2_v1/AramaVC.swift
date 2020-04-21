213ffa85bbb536b3ec42f5f6b279fe5faece002d
Ömer Alışkan <omerfarukaliskan@icloud.com>
2020-04-21 21:11:37 +0300

    Arama Sayfası lbl txt btn tableview tanımlandı

--- proje2_v1/AramaVC.swift
+++ proje2_v1/AramaVC.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class AramaVC: UIViewController {
-
+class AramaVC: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var AramaLbl: UILabel!
+    @IBOutlet weak var AramaTxt: UITextField!
+    @IBOutlet weak var aramTableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        aramTableview.delegate = self
+        aramTableview.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func AraBtnClicked(_ sender: Any) {
     }
-    */
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
 }
