8de9d27e17f823d737a0139370121ae268a242aa
Ömer Alışkan <omerfarukaliskan@icloud.com>
2020-05-24 13:00:02 +0300

    Chat ve Message modelleri extensionları ile birlikte eklendi

--- proje2_v1/Models/Messages/chat.swift
+++ proje2_v1/Models/Messages/chat.swift
@@ -9,4 +9,22 @@
 import Foundation
 import UIKit
 
+struct Chat {
+    
+    var users: [String]
+    
+    var dictionary: [String: Any] {
+        return [
+            "users": users
+        ]
+    }
+}
 
+extension Chat {
+    
+    init?(dictionary: [String:Any]) {
+        guard let chatUsers = dictionary["users"] as? [String] else {return nil}
+        self.init(users: chatUsers)
+    }
+    
+}
